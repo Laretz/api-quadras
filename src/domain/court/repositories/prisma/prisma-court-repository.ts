1ed8d353b39c1c9d2526083983f408176a724f81
Arthur Cds <arthuralvesoliveira@outlook.com>
2025-06-13 00:20:25 -0300

    adicionado delete e getall de quadra

--- src/domain/court/repositories/prisma/prisma-court-repository.ts
+++ src/domain/court/repositories/prisma/prisma-court-repository.ts
@@ -5,6 +5,18 @@ import { CourtRepository } from "../court-repository";
 
 
 export class PrismaCourtRepository implements CourtRepository{
+    async delete(id: string): Promise<void> {
+        await prisma.court.delete({
+            where: {id}
+        })
+        
+    }
+    async getAllCourts(): Promise<CourtReturnFunctionRepository["getCourt"][]> {
+        const courts = await prisma.court.findMany({
+            include: {reservas: true}
+        })
+        return courts
+    }
     async create(court: CourtParamsFunctionRepository["createCourt"]): Promise<CourtReturnFunctionRepository["getCourt"]> {
         const createdCourt = await prisma.court.create({
             data: {
--- src/domain/court/types/params/court.ts
+++ src/domain/court/types/params/court.ts
@@ -2,6 +2,7 @@ import { Prisma } from "@prisma/client"
 
 export interface CourtParamsFunctionRepository{
     createCourt: Prisma.CourtCreateInput,
-    updateCourt: Prisma.CourtUncheckedUpdateInput
+    updateCourt: Prisma.CourtUncheckedUpdateInput,
+    
 
 }
